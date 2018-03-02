763650438c99aae1659d450c0f700b1814594fbf
Josh Gulledge <jgulledge19@outlook.com>
2018-03-02 06:03:13 -0500

    Update test/sample.config.php

--- tests/sample.config.php
+++ tests/sample.config.php
@@ -1,6 +1,22 @@
 <?php
-// Need to define the path the MODX core config file
-require_once '/www/config.core.php';
+/** @dependency \modX */
+
+// Define where MODX is:
+define('MODX_PATH', '/www/');
+
+$config =  MODX_PATH.'/config.core.php';
+
+if (file_exists($config)) {
+    require_once $config;
+} else {
+    $config = MODX_PATH . 'core/config/config.inc.php';
+    require_once $config;
+}
+
+$modx_class = MODX_CORE_PATH;
+if (!file_exists($modx_class)) {
+    die('The '.$config.' is not correct, could not find the MODX class in: '.$modx_class);
+}
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 
 define('BLEND_MODX_MIGRATION_PATH', dirname(__DIR__).'/tests/');
