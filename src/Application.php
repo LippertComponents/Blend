b1c058d6bfcf440e98584726e8b129faba2d8bfb
Josh Gulledge <jgulledge19@outlook.com>
2018-09-25 09:15:03 -0400

    Update to beta9

--- src/Application.php
+++ src/Application.php
@@ -19,7 +19,7 @@ class Application extends ConsoleApplication
 
     protected static $name = 'Blend Console';
 
-    protected static $version = '1.0.0 beta6';
+    protected static $version = '1.0.0 beta9';
 
     /**
      * Gets the default input definition.
