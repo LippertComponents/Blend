a2bcba08c2846b8e1be871297993a16752705c70
Josh Gulledge <jgulledge19@outlook.com>
2019-03-16 17:20:24 -0400

    Add BlendableKeyLengthException class

---
+++ src/Exception/BlendableKeyLengthException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshgulledge
+ * Date: 10/3/18
+ * Time: 2:19 PM
+ */
+
+namespace LCI\Blend\Exception;
+
+
+class BlendableKeyLengthException extends \Exception
+{
+
+}
